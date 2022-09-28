2ddca62fc0422e7d1f6ef24ba4c89fe1d080aa89
Mohanish19 <91741715+Mohanish19@users.noreply.github.com>
2022-09-28 11:51:02 +0530

    Add files via upload

---
+++ qnabothealthawesome-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\qnabothealthawesome-bot-85d6-Web-Deploy.pubxml -p:Password=xCozPekimsrfiHcQhNlQ60o3bCZkLcaff2FnHtDztRh9Zhr5dD8QAn9q1rv1
+
